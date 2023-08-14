b41862faf2a1bacaef1a7c23a965196e79dc9d8a
aidenhu <99224270+CS2Student@users.noreply.github.com>
2023-08-14 19:44:57 -0400

    Setup GET resources api endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,18 @@ app.get("/", (req, res) => {
     res.json("hello this is the backend");
 });
 
+// GET (READ) resources 
+app.get("/resources", (req, res) => {
+    const query = "SELECT * FROM resources";
+    db.query(query, (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json(err); // Internal Server Error
+        }
+        return res.status(200).json(data); // OK
+    });
+});
+
 app.listen(8800, () => {
     console.log("Connected to backend");
 });
