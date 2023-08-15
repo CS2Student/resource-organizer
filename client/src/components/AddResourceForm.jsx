bc210101519b13e1791e3d98594224dbbe157236
aidenhu <99224270+CS2Student@users.noreply.github.com>
2023-08-15 01:47:24 -0400

    Implement form input handling and submission functionality

--- client/src/components/AddResourceForm.jsx
+++ client/src/components/AddResourceForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const AddResourceForm = () => {
@@ -15,15 +15,32 @@ const AddResourceForm = () => {
 
     const navigate = useNavigate();
 
+    // Update resource properties on user input
+    const handleChange = (e) => {
+        setResource(prevResource => ({ ...prevResource, [e.target.name]: e.target.value}));
+    }
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post('http://localhost:8800/resources', resource);
+            navigate("/");
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
     return (
         <>
             <h1>Add New Resource</h1>
-            <input type='text' placeholder='title' name='title'/>
-            <input type='text' placeholder='description' name='description'/>
-            <input type='text' placeholder='type' name='type'/>
-            <input type='text' placeholder='category' name='category'/>
-            <input type='text' placeholder='sub_category' name='sub category'/>
-            <input type='text' placeholder='link' name='link'/>
+            <input type='text' placeholder='title' onChange={handleChange} name='title'/>
+            <input type='text' placeholder='description' onChange={handleChange} name='description'/>
+            <input type='text' placeholder='type' onChange={handleChange} name='type'/>
+            <input type='text' placeholder='category' onChange={handleChange} name='category'/>
+            <input type='text' placeholder='sub category' onChange={handleChange} name='sub_category'/>
+            <input type='text' placeholder='link' onChange={handleChange} name='link'/>
+            <button className="formButton" onClick={handleClick}>Save</button>
         </>
     )
 }
