6729e105b3dbfe1adf6f12dfd25146d617d1bb67
aidenhu <99224270+CS2Student@users.noreply.github.com>
2023-08-15 23:06:38 -0400

    Add placeholders for empty form fields

--- client/src/pages/UpdateResourceForm.jsx
+++ client/src/pages/UpdateResourceForm.jsx
@@ -59,12 +59,12 @@ const UpdateResourceForm = () => {
         <>
             <h1 className="form-title">Update Resource</h1>
             <form className="form-container">
-                <input type='text' value={resource.title} onChange={handleChange} name='title'/>
-                <input type='text' value={resource.description} onChange={handleChange} name='description'/>
-                <input type='text' value={resource.type} onChange={handleChange} name='type'/>
-                <input type='text' value={resource.category} onChange={handleChange} name='category'/>
-                <input type='text' value={resource.sub_category} onChange={handleChange} name='sub_category'/>
-                <input type='text' value={resource.link} onChange={handleChange} name='link'/>
+                <input type='text' placeholder="title" value={resource.title} onChange={handleChange} name='title'/>
+                <input type='text' placeholder="description" value={resource.description} onChange={handleChange} name='description'/>
+                <input type='text' placeholder="type" value={resource.type} onChange={handleChange} name='type'/>
+                <input type='text' placeholder="category" value={resource.category} onChange={handleChange} name='category'/>
+                <input type='text' placeholder="sub category" value={resource.sub_category} onChange={handleChange} name='sub_category'/>
+                <input type='text' placeholder="link" value={resource.link} onChange={handleChange} name='link'/>
                 <button className="button" onClick={handleCancel}>Cancel</button>
                 <button className="button" onClick={handleSave}>Save</button>
             </form>
